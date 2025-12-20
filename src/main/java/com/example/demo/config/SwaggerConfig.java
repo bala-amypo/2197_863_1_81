7ea466ca5ed005f3dd6fb0157a918a329a0b00c7
bala-amypo <bala@amypo.in>
2025-12-20 09:41:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme scheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Digital Asset Lifecycle API")
+                        .version("1.0"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", scheme));
+    }
+}
