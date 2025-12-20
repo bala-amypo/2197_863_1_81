4a62dd23fd64ad8b9fe5714d3dc2112809e357e7
bala-amypo <bala@amypo.in>
2025-12-20 11:26:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,29 +1,29 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.*;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.security.*;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
+//     @Bean
+//     public OpenAPI openAPI() {
 
-        SecurityScheme scheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
+//         SecurityScheme scheme = new SecurityScheme()
+//                 .type(SecurityScheme.Type.HTTP)
+//                 .scheme("bearer")
+//                 .bearerFormat("JWT");
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Asset Lifecycle API")
-                        .version("1.0"))
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", scheme));
-    }
-}
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Digital Asset Lifecycle API")
+//                         .version("1.0"))
+//                 .addSecurityItem(
+//                         new SecurityRequirement().addList("bearerAuth"))
+//                 .components(new Components()
+//                         .addSecuritySchemes("bearerAuth", scheme));
+//     }
+// }
