625896f476972eba37054abc6979c087c4eed498
bala-amypo <bala@amypo.in>
2025-12-22 03:32:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/DisposalRecordRepository.java
+++ src/main/java/com/example/demo/repository/DisposalRecordRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.entity.DisposalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DisposalRecordRepository extends JpaRepository<DisposalRecord, Long> {
+   
 }
