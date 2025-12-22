4f358d8721436bf89af180c8789d4743d35747c7
bala-amypo <bala@amypo.in>
2025-12-22 03:35:59 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetService.java
+++ src/main/java/com/example/demo/service/AssetService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.*;
+import com.example.demo.entity.Asset;
 import java.util.List;
 
 public interface AssetService {
+
     Asset createAsset(Asset asset);
+
     Asset getAsset(Long id);
+
     List<Asset> getAllAssets();
-    Asset updateStatus(Long id, String status);
+
+    Asset updateStatus(Long assetId, String status);
 }
